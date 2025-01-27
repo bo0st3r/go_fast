4f9d1b439de53e4243d8e6274f5c5e727eede310
bo0st3r <bastiendct@gmail.com>
2025-01-26 23:30:55 -0500

    feat: add docker & move main to cmd/server

--- Makefile
+++ Makefile
@@ -3,8 +3,19 @@ HOST = http://localhost:8080
 GREEN = \033[0;32m
 NC = \033[0m # No Color
 
-.PHONY: health
+.PHONY: health buildstart db-up db-down
+
+buildstart:
+	rm -f ./go-fast
+	go build -o go-fast cmd/server/main.go
+	./go-fast 
 
 health: 
 	@echo "${GREEN}How's that thang goin'?${NC}"
 	curl ${HOST}/health
+
+db-up:
+	docker compose -f docker/docker-compose.yml --env-file .env up
+
+db-down:
+	docker compose -f docker/docker-compose.yml --env-file .env down
---
+++ cmd/server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+
+	"github.com/bo0st3r/go-fast/internal/config"
+	"github.com/bo0st3r/go-fast/internal/db"
+	"github.com/bo0st3r/go-fast/internal/telemetry"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	log.Println("Warming up the machine")
+
+	// Load the .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	config := config.Load()
+	log.Println("Config loaded.")
+
+	db_connection, err := db.Connect(config.DatabaseDSN)
+	if err != nil {
+		log.Fatal("Error connecting to DB!", err)
+	}
+	defer db_connection.Close()
+	log.Println("Connected to DB.")
+
+	telemetryRepository := telemetry.NewRepository(db_connection)
+	telemetryService := telemetry.NewService(telemetryRepository)
+	telemetryHandler := telemetry.NewHandler(telemetryService)
+
+	r := setupRouter(telemetryHandler)
+
+	http.ListenAndServe(":8080", r)
+	log.Println("She's ready!")
+}
+
+func setupRouter(telemetryHandler *telemetry.Handler) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Abusing HTTP 302 to grow my network 😎
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "https://github.com/bo0st3r", http.StatusFound)
+	})
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("I'm fine sir!!"))
+	})
+
+	r.Get("/telemetry", telemetryHandler.GetAll)
+	r.Post("/telemetry", telemetryHandler.Create)
+
+	return r
+}
